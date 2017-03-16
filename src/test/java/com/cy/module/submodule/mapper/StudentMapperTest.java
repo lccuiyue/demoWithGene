2efbeddd13d3a9ccc585add92230538399b22e09
leapingcui <cuiyue2017@gmail.com>
2017-03-16 23:13:01 +0800

    初步体验

---
+++ src/test/java/com/cy/module/submodule/mapper/StudentMapperTest.java
@@ -0,0 +1,13 @@
+package com.cy.module.submodule.mapper;
+
+import com.cy.module.submodule.BaseTest;
+
+/**
+ * Created by Administrator on 2017/3/16.
+ */
+public class StudentMapperTest extends BaseTest {
+
+
+
+
+}
